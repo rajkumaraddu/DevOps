176c8d23e780fe2390e6fd94af7bc3bdcbdb5502
rajkumaraddu <81338791+rajkumaraddu@users.noreply.github.com>
2021-03-29 23:57:31 +0530

    initial commit

---
+++ src/test/java/com/mahindra/otpapp/OtpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahindra.otpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
