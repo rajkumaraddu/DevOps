61d37cc0805bff3acdd995632422cfc84b81d07e
rajkumaraddu <81338791+rajkumaraddu@users.noreply.github.com>
2021-03-30 00:44:32 +0530

    Update OtpAppApplication.java

--- src/main/java/com/mahindra/otpapp/OtpAppApplication.java
+++ src/main/java/com/mahindra/otpapp/OtpAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OtpAppApplication {
+public class OtpAppApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(OtpAppApplication.class, args);
